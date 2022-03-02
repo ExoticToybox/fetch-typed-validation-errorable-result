ac7c7871bdb8e83f90f6cd81de43f2dcb5b0a3f3
kawashima <pyjama.kud@gmail.com>
2022-03-02 17:27:55 +0900

    chore: add prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
