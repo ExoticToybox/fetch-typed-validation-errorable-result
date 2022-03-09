09415e7316d8dcfd643afaaba853eecb0b23bac4
ExoticToybox <pyjama.kud@gmail.com>
2022-03-09 17:12:36 +0900

    chore: add eslint-plugin-import
    (cherry picked from commit 8fe42af984b3c05758cf4c33cd23fe59afe2e69f)

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,24 @@ module.exports = {
         tsx: 'never',
       },
     ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
     'react/jsx-filename-extension': [
       'error',
       {
